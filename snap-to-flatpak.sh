690f1f0210f988eb158cdfbe22489719c05fb126
MasterGeekMX <paclarushtaonas@gmail.com>
2024-01-10 23:25:02 -0600

    additions, corrections and improvements in comments

--- snap-to-flatpak.sh
+++ snap-to-flatpak.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-# snap-to-flat script Version 1.1
+# snap-to-flat script Version 1.2
 # A BASH script that removes Snap from an Ubuntu system and replaces it with Flatpak
 # By MasterGeek.MX
 
 # There is a saying: "Do to others as you would have them do to you" (Matthew 7:12),
-# So this code, as my other works, is heavly commented and explained so others
+# So this code, as my other works, is heavily commented and explained so others
 # can learn from it, no matter their level.
 
-# Before everything, a warning and a prompt for good measure.
-# Oh, and all those 'tput' things are for switching between bold and normal text
-
+# Let's start by issuing a warning
+# all those 'tput' things are for switching between bold text and normal text (sgr0)
 tput bold
 echo ""
 echo "----------------------------------------------"
@@ -19,7 +18,7 @@ echo " REPEAT: ALL YOUR SNAPS WILL BE DELETED!!!!!!!"
 echo "----------------------------------------------"
 echo ""
 
-#ring 3 times the terminal by sending the bell character
+# ring 3 times the terminal by sending the bell character
 for i in {1..3}
 do
 	# -e tells echo to read the Escape characters.
@@ -28,8 +27,10 @@ do
 	sleep .2
 done
 
+# prompt the user if they wish to continue
 read -p "Are you sure [y/n]: " confirmation
 
+# if the answer was negative, don't proceed and exit
 if [[ ! $confirmation =~ [Yy](es)?$ ]]
 then
 	echo -e "\nOK, nothing was done.\n"
@@ -38,6 +39,8 @@ then
 	exit
 fi
 
+# unsetting variables (removing them) ensures no variables are left behind after the script.
+# it is only a good practice, but not mandatory.
 unset $confirmation
 
 echo -e "\nOK. Proceeding..."
@@ -50,14 +53,24 @@ tput sgr0
 
 snaps=$(snap list | tail --lines +2 | cut --field 1 --delimiter " ")
 
-#now some hoops because `snaps` isn't a proper bash array (for some reason i don't know yet)
+# now some hoops to put all snaps in a proper bash array because `snaps`
+# isn't a proper bash array (for some reason i don't know yet)
+
+# create an empty bash array
 declare -a snap_list=()
 
+# go element by element on the snaps
 for snap in $snaps
 do
+	# and add them to the snaps array
 	snap_list+=($snap)
 done
 
+# save in a variable how many snaps we have by the count of elements in the snaps list array
+snap_count=${#snap_list[@]}
+
+# print the number and list of snaps installed
+#TODO print the number
 tput bold
 echo -e "\nThe installed snaps are:"
 tput sgr0
@@ -65,41 +78,39 @@ echo -e "${snap_list[@]}\n"
 
 # Now we are going through that array and remove each snap package from it.
 # Snap doesn't let you remove a snap that is a dependency, and unfortunately,
-# snap doesn't offer an option (that I know) to list dependencies, so we are
-# going to iterate over the array and try each snap. If it was removed,
-# then we pop it out of the list abd continue, then we start all over again.
-# I know it is not efficient, but it is what I have for the moment.
+# snaps doesn't offer an option (that I know) to list dependencies, so we are
+# going to iterate over the array and try to remove each snap. If it was removed successfully,
+# then we pop it out of the list and continue to the next, then we start all over again.
+# I know this solution isn't efficient, but it is what I have for the moment.
 
-snap_count=${#snap_list[@]}
-
-#while the list of snaps isn"t empty...
+# while the list of snaps isn't empty...
 while [[ $snap_count -gt 0 ]]
 do
-	#move an index in the range of elements on the snap list
-	for ((index=0; index<snap_count; index++)) #index in ${!snap_list[@]}
+	# move an index in the range of elements on the snap list
+	for ((index=0; index<snap_count; index++))
 	do
-		#keep track of the snap we are working on in a variable with an easier name
+		# keep track of the snap we are working on currently in a variable with a name easier to read
 		current_snap=${snap_list[$index]}
-		#remove the snap indicated by the index, and discard the error message if it couldn't be removed
+		# remove the snap indicated by the index, and discard the error message if it couldn't be removed
 		sudo snap remove --purge $current_snap 2> /dev/null
-		#if the last command was successful (the snap could be removed)...
+		# if the last command was successful by exiting with status zero (meaning the snap could be removed)...
 		if [[ $? -eq 0 ]]
-		then
-			#create a new empty list for the updated snap list
+		then # update the list of snaps by removing the one we just uninstalled
+			# create a new empty list for the updated snap list
 			declare -a new_snap_list=()
-			#go in each snap on the list
+			# iterate on each snap on the list
 			for snap in ${snap_list[@]}
 			do
-				#if the snap in the list isn't the one we removed...
+				# if the snap in the list isn't the one we just removed...
 				if [[ $snap != $current_snap ]]
 				then
-					#add the snap into the new list
+					# add the snap into the new list
 					new_snap_list+=($snap)
 				fi
 			done
-			#replace the old list with the updated one
+			# replace the old list with the updated one
 			snap_list=(${new_snap_list[@]})
-			#and update the count of snaps
+			# and update the count of snaps
 			snap_count=${#snap_list[@]}
 		fi
 	done
@@ -110,28 +121,35 @@ echo -e "\nall snaps were removed"
 echo -e "\nSECOND STEP: Directory cleanup"
 
 # Now we are going to remove files left by snaps.
-# It is easier to make a list of possible directories.
+# It is easier to make a list of possible directories than trying to search them dynamically.
 # Contributors: feel free to add others that I missed.
 
+# let's create a list of the directories to be removed
 declare -a directories=("$HOME/snap" "/snap" "/var/snap" "/var/lib/snap" "/var/cache/snapd" "/usr/lib/snapd")
 
+# iterate on each directory on the list of directories...
 for directory in ${directories[@]}
 do
-	if [[ -d $directory ]] #check if directory exists
+	# if the path we are looking up exist in the filesystem as a directory...
+	if [[ -d $directory ]]
 	then
+		# ...ask the user if they want to remove the directory and it's contents completely
 		echo ""
 		read -p "Do you want to completely remove the directory $directory? [y/n]: " rm_confirmation
 
+		# check the user's answer and either do nothing or proceed with the removal
 		if [[ ! $rm_confirmation =~ [Yy](es)?$ ]]
 		then
+			# answer was negative. Don't remove the directory
 			echo -e "\nOK. $directory is left untouched."
 		else
+			# answer was affirmative. Remove the directory completely
 			echo -e "\nRemoving $directory\n"
 			tput sgr0
-			# -r tells rm to be Recursive; betting inside each directory and removing stuff inside
-			# -f tells rm to don't ask about removing a file and Forces it's removal
-			# -v tells rm to be Verbose and print each thing it deletes.
-			sudo rm -rfv "$directory"
+			# --recursive tells rm to get inside each subdirectory and remove everything inside
+			# --force tells rm to don't ask about removing a file and Forces it's removal
+			# --verbose tells rm to print each thing it deletes.
+			sudo rm --recursive --force --verbose "$directory"
 			tput bold
 		fi
 
@@ -150,18 +168,21 @@ echo -e "\nTHIRD STEP: Deactivation of snap"
 echo -e "\nStopping and deactivating snap services...\n"
 tput sgr0
 
-sudo systemctl stop snapd.socket -T
-sudo systemctl stop snapd.service -T
-sudo systemctl disable snapd.service
+# systemctl is the program that allows service management.
+# stopping a service means halting it on the spot.
+# the --show-transaction is only to make it more verbose
+sudo systemctl stop snapd.socket --show-transaction
+sudo systemctl stop snapd.service --show-transaction
 
 tput bold
 echo -e "\nRemoving and holding snap...\n"
 tput sgr0
 
-#we use autoremove because it will also remove other releated packages.
-#purge is for also removing config files.
+# we use autoremove because it will also remove other related packages.
+# the --purge options is for also removing configuration files,
+# and the --assume-yes to do the uninstalling automatically instead of asking the user to confirm
 sudo apt autoremove --purge snapd --assume-yes
-#marking a package as held prevents it from being installed/uninstalled
+# marking a package as held prevents it from being installed/uninstalled. Just in case.
 sudo apt-mark hold snapd
 
 tput bold
@@ -177,29 +198,35 @@ sudo apt install flatpak --assume-yes
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # we are going to detect if the user has a GUI app store compatible with Flatpak
-# (currently only GNOME software and KDE's Discover support flatpak).
+# (currently only GNOME Software and KDE's Discover support flatpak).
 # If the user does not have neither installed, a prompt suggesting them will appear.
 
-#detect if gnome-software or plasma-discover is available
-if [[ -x $(command -v plasma-discover) ]]
-then
-	appstore="discover"
-elif [[ -x $(command -v gnome-software) ]]
+# detect if gnome-software or plasma-discover is already installed:
+# the `command` command is for running stuff that is not bash functions, but if
+# we pass the -v flag instead it prints the path where the program lives, and nothing if not found.
+# then we check with -x if that path is an executable file. If all is correct, we have found our app store.
+#TODO change the variables to the project name (GNOME and KDE)
+if [[ -x $(command -v gnome-software) ]]
 then
 	appstore="software"
+elif [[ -x $(command -v plasma-discover) ]]
+then
+	appstore="discover"
 else
 	appstore="none"
 fi
 
-#launch the prompt in case there is no app store
+# launch the prompt in case we didn't find a suitable app store
 if [[ $appstore == "none" ]]
 then
 	tput bold
 	echo -e "\nYou can install flatpaks with the terminal, but there is the option"
 	echo "of using a graphical App Store to do it more comfortably."
 
+	# ask the user if they want to install an app store
 	read -p "Do you want to install an App Store program? [y/n]: " use_appstore
 
+	# check the answer to see if we proceed with the app store selection or not.
 	if [[ $use_appstore =~ [Yy](es)?$ ]]
 	then
 		echo -e "\nNeat! the options are GNOME Software and KDE Discover"
@@ -208,18 +235,13 @@ then
 		echo "and GNOME Software for pretty much everything else:"
 		echo "GNOME (regular Ubuntu), xfce (Xubuntu), Mate (Ubuntu Mate/Kylin), Budgie (Ubuntu Budgie)..."
 
+		# prompt the user with the choices of app store available as numbers
 		echo -e "\nSelect the number of the App Store you would like to have:"
-		select appstore_to_install in KDE-Discover GNOME-Software
+		select appstore_to_install in GNOME-Software KDE-Discover
+
+		# install the app store the user selected based on it's answer
 		do
 			case $appstore_to_install in
-				"KDE-Discover")
-					echo -e "\nOK. Installing KDE Discover...\n"
-					tput sgr0
-					sudo apt install plasma-discover --assume-yes
-					appstore="discover"
-					tput bold
-					break
-				;;
 				"GNOME-Software")
 					echo -e "\nOK. Installing GNOME Software...\n"
 					tput sgr0
@@ -228,36 +250,45 @@ then
 					tput bold
 					break
 				;;
-				*)
+								"KDE-Discover")
+					echo -e "\nOK. Installing KDE Discover...\n"
+					tput sgr0
+					sudo apt install plasma-discover --assume-yes
+					appstore="discover"
+					tput bold
+					break
+				;;
+				*) # the user didn't select a number that corresponds with a valid choice
 					echo "That is not an option!"
 				;;
 			esac
 		done
-	else
+	else # the user said no to installing an app store
 		echo -e "\nOK then. No GUI app will be installed."
 	fi
 fi
 
-#install the corresponding backend for flatpaks
-
+#install the corresponding backend for flatpaks for the installed app store
+# check if there is even an app store to work with in the first place
 if [[ $appstore != "none" ]]
 then
 	tput bold
 	echo -e "\nInstalling the corresponding flatpak backend...\n"
 	tput sgr0
-	if [[ $appstore == "discover" ]]
-	then
-		tput sgr0
-		sudo apt install plasma-discover-backend-flatpak --assume-yes
-		tput bold
-	elif [[ $appstore == "software" ]]
+	if [[ $appstore == "software" ]]
 	then
 		tput sgr0
 		sudo apt install gnome-software-plugin-flatpak --assume-yes
 		tput bold
+	elif [[ $appstore == "discover" ]]
+	then
+		tput sgr0
+		sudo apt install plasma-discover-backend-flatpak --assume-yes
+		tput bold
 	fi
 fi
 
+# finally, tell the user we have finished
 echo -e "\nWe are done! snap is no more, and flatpak is in.\n"
 echo "check the list of the apps that were installed as snap in case"
 echo "you want them reinstalled in flatpak/apt format, like Firefox."
